1fe5be83296f951decf767a54bef4473150934a1
jde-agr <jde-agr@student.wethinkcode.co.za>
2019-10-08 11:22:03 +0200

    ex01 for day00

---
+++ day00/ex00/ex00/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by jasmine DE-AGRELA on 2019/10/08.
+//  Copyright © 2019 jde-agr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickMeTapped(_ sender: Any) {
+        print("Hello World !")
+    }
+    
+}
+
---
+++ day00/ex01/ex01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ex01
+//
+//  Created by jasmine DE-AGRELA on 2019/10/08.
+//  Copyright © 2019 jde-agr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
