98d6effc477c4cfa569cd3104d83ad030eea0b72
jde-agr <jde-agr@student.wethinkcode.co.za>
2019-10-14 14:37:32 +0200

    Part02 of day05 implemented

--- day05/day05/day05/SecondViewController.swift
+++ day05/day05/day05/SecondViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
-class SecondViewController: UIViewController {
+//Lat -33.90740025      -33.9071
+//Long 18.41660453      18.4173
 
+class CustomPin: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(pinTitle: String, pinSubTitle: String, location: CLLocationCoordinate2D) {
+        self.title = pinTitle
+        self.subtitle = pinSubTitle
+        self.coordinate = location
+    }
+}
+
+class SecondViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let location = CLLocationCoordinate2D(latitude: -33.9071, longitude: 18.4173)
+        let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+        self.mapView.setRegion(region, animated: true)
+        let pin = CustomPin(pinTitle: "WeThinkCode_ Cape Town", pinSubTitle: "Cape Town Campus", location: location)
+        self.mapView.addAnnotation(pin)
+        self.mapView.delegate = self
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "customannotation")
+        annotationView.image = UIImage(named: "pin_blue")
+        annotationView.canShowCallout = true
+        return annotationView
     }
 
 
