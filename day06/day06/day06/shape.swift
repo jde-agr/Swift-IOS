c98d0142cacd6f4e550b59e9370f991effb8dbb9
jde-agr <jde-agr@student.wethinkcode.co.za>
2019-10-15 16:34:55 +0200

    Final version of rush00

---
+++ day06/day06/day06/shape.swift
@@ -0,0 +1,21 @@
+//
+//  shape.swift
+//  day06
+//
+//  Created by jasmine DE-AGRELA on 2019/10/15.
+//  Copyright © 2019 jde-agr. All rights reserved.
+//
+
+import UIKit
+
+class shape: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
